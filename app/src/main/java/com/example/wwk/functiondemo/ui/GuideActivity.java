b1d771921762958cc29a2def527aee4260b9445e
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-21 15:02:54 +0900

    Add LaunchActivity and GuideActivity class, then implement functions that delay launch and judge whether or not first launch if it's first then get into guide page

---
+++ app/src/main/java/com/example/wwk/functiondemo/ui/GuideActivity.java
@@ -0,0 +1,21 @@
+package com.example.wwk.functiondemo.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.wwk.functiondemo.R;
+
+/**
+ * Created by wwk on 17/5/21.
+ *
+ * This is a guide page When users are using this app firstly.
+ */
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
---
+++ app/src/main/java/com/example/wwk/functiondemo/ui/LaunchActivity.java
@@ -0,0 +1,75 @@
+package com.example.wwk.functiondemo.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.wwk.functiondemo.MainActivity;
+import com.example.wwk.functiondemo.R;
+import com.example.wwk.functiondemo.utils.SharedPreferencesUtils;
+
+/**
+ * Created by wwk on 17/5/21.
+ *
+ * Interface of Launch
+ */
+
+public class LaunchActivity extends AppCompatActivity{
+
+    // Estimate app that is it being launched for the first time
+    public static final String SHARE_IS_FIRST = "isFirst";
+    // Delay of launch
+    public static final int HANDLER_LAUNCH = 1001;
+    private TextView mLaunchView;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case HANDLER_LAUNCH:
+                    // Estimate app that is it being launched for the first time
+                    if (isFirst()) {
+                        startActivity(new Intent(LaunchActivity.this, GuideActivity.class));
+                    } else {
+                        startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                    }
+                    finish();
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        initializeView();
+    }
+
+    // Initialize view
+    private void initializeView() {
+        // Delay 2 second when launching the interface
+        handler.sendEmptyMessageDelayed(HANDLER_LAUNCH, 2000);
+        mLaunchView = (TextView) findViewById(R.id.launch_view);
+    }
+
+    // Estimate app that is it being launched for the first time
+    private boolean isFirst() {
+        boolean isFirst = SharedPreferencesUtils.getBoolean(this, SHARE_IS_FIRST, true);
+
+        if (isFirst) {
+            SharedPreferencesUtils.putBoolean(this, SHARE_IS_FIRST, false);
+            // It's first Launching
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
