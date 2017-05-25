4930b280e7527b9fd1b579386fffb9282efc1d2d
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-25 15:18:25 +0900

    Implement function of update profile and optimize layout

--- app/src/main/java/com/example/wwk/functiondemo/fragment/FragmentDemoFour.java
+++ app/src/main/java/com/example/wwk/functiondemo/fragment/FragmentDemoFour.java
@@ -3,12 +3,14 @@ package com.example.wwk.functiondemo.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wwk.functiondemo.R;
 import com.example.wwk.functiondemo.entity.MyUser;
@@ -17,6 +19,8 @@ import com.example.wwk.functiondemo.utils.L;
 
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
 
 /**
  * Created by wwk on 17/5/18.
@@ -35,7 +39,10 @@ public class FragmentDemoFour extends Fragment implements View.OnClickListener {
     private EditText mEditProfileGender;
     private EditText mEditProfileAge;
     private EditText mEditProfileDescription;
-    
+
+    // button of Update profile
+    private Button mUpdateProfileButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_demo_four, container, false);
@@ -51,6 +58,8 @@ public class FragmentDemoFour extends Fragment implements View.OnClickListener {
         mExitLoginButton.setOnClickListener(this);
         mEditProfileText = (TextView) view.findViewById(R.id.edit_profile);
         mEditProfileText.setOnClickListener(this);
+        mUpdateProfileButton = (Button) view.findViewById(R.id.update_profile_button);
+        mUpdateProfileButton.setOnClickListener(this);
 
         mEditProfileName = (EditText) view.findViewById(R.id.edit_username_profile);
         mEditProfileGender = (EditText) view.findViewById(R.id.edit_gender_profile);
@@ -92,6 +101,54 @@ public class FragmentDemoFour extends Fragment implements View.OnClickListener {
 
             // Edit profile
             case R.id.edit_profile:
+                setEnabled(true);
+                mUpdateProfileButton.setVisibility(View.VISIBLE);
+                break;
+
+            // Update profile
+            case R.id.update_profile_button:
+                // Get values from edit
+                String name = mEditProfileName.getText().toString();
+                String age = mEditProfileAge.getText().toString();
+                String gender = mEditProfileGender.getText().toString();
+                String description = mEditProfileDescription.getText().toString();
+
+                // Estimate input whether or not is empty
+                if (!TextUtils.isEmpty(name) & !TextUtils.isEmpty(age) & !TextUtils.isEmpty(gender)) {
+                    // Update
+                    MyUser user = new MyUser();
+                    user.setUsername(name);
+                    user.setAge(Integer.parseInt(age));
+                    // Gender
+                    if (gender.equals("male")) {
+                        user.setGender(true);
+                    } else {
+                        user.setGender(false);
+                    }
+                    // Description
+                    //简介
+                    if (!TextUtils.isEmpty(description)) {
+                        user.setIntroduction(description);
+                    } else {
+                        user.setIntroduction("This man is lazy and nothing is left.");
+                    }
+
+                    BmobUser bmobUser = BmobUser.getCurrentUser();
+                    user.update(bmobUser.getObjectId(), new UpdateListener() {
+                        @Override
+                        public void done(BmobException e) {
+                            if (e == null) {
+                                setEnabled(false);
+                                mUpdateProfileButton.setVisibility(View.GONE);
+                                Toast.makeText(getActivity(), "Successful Update", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getActivity(), "Failure to Update", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getActivity(), "Input can not be empty", Toast.LENGTH_LONG).show();
+                }
                 break;
         }
     }
