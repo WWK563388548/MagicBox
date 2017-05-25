f84ecfa503c3a697df329bb6b1448ba2e3db51f3
wc563388548@gmail.com <wc563388548@gmail.com>
2017-05-25 14:18:12 +0900

    update function of FragmentDemoFour

--- app/src/main/java/com/example/wwk/functiondemo/fragment/FragmentDemoFour.java
+++ app/src/main/java/com/example/wwk/functiondemo/fragment/FragmentDemoFour.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.wwk.functiondemo.R;
@@ -29,6 +30,11 @@ public class FragmentDemoFour extends Fragment implements View.OnClickListener {
     public static final String BMOB_APP_ID = "4d345003be01bddf52e5e1bfbe0f2ea2";
     private Button mExitLoginButton;
     private TextView mEditProfileText;
+
+    private EditText mEditProfileName;
+    private EditText mEditProfileGender;
+    private EditText mEditProfileAge;
+    private EditText mEditProfileDescription;
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,6 +49,29 @@ public class FragmentDemoFour extends Fragment implements View.OnClickListener {
 
         mExitLoginButton = (Button) view.findViewById(R.id.exit_login_button);
         mExitLoginButton.setOnClickListener(this);
+        mEditProfileText = (TextView) view.findViewById(R.id.edit_profile);
+        mEditProfileText.setOnClickListener(this);
+
+        mEditProfileName = (EditText) view.findViewById(R.id.edit_username_profile);
+        mEditProfileGender = (EditText) view.findViewById(R.id.edit_gender_profile);
+        mEditProfileAge = (EditText) view.findViewById(R.id.edit_age_profile);
+        mEditProfileDescription = (EditText) view.findViewById(R.id.edit_description_profile);
+
+        // Can not edit profile that is default
+        setEnabled(false);
+        // Set value of default
+        MyUser userInfo = BmobUser.getCurrentUser(MyUser.class);
+        mEditProfileName.setText(userInfo.getUsername());
+        mEditProfileAge.setText(userInfo.getAge() + "");
+        mEditProfileGender.setText(userInfo.isGender() ? "male" : "female");
+        mEditProfileDescription.setText(userInfo.getIntroduction());
+    }
+
+    private void setEnabled(boolean is) {
+        mEditProfileName.setEnabled(is);
+        mEditProfileAge.setEnabled(is);
+        mEditProfileGender.setEnabled(is);
+        mEditProfileDescription.setEnabled(is);
     }
 
     @Override
@@ -59,6 +88,10 @@ public class FragmentDemoFour extends Fragment implements View.OnClickListener {
                 L.information("currentUser's value: " + currentUser);
                 startActivity(new Intent(getActivity(), LoginActivity.class));
                 getActivity().finish();
+                break;
+
+            // Edit profile
+            case R.id.edit_profile:
                 break;
         }
     }
