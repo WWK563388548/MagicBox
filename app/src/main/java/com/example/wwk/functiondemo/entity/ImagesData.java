a09248a8dd6bca41dd0a5691af88ace2c391e762
wc563388548@gmail.com <wc563388548@gmail.com>
2017-06-03 12:36:29 +0900

    Add layout of FragmentDemoThree and entity class of  image's data

---
+++ app/src/main/java/com/example/wwk/functiondemo/entity/ImagesData.java
@@ -0,0 +1,19 @@
+package com.example.wwk.functiondemo.entity;
+
+/**
+ * Created by wwk on 17/6/3.
+ */
+
+public class ImagesData {
+
+    // Url
+    private String mImageUrl;
+
+    public String getmImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setmImageUrl(String mImageUrl) {
+        this.mImageUrl = mImageUrl;
+    }
+}
