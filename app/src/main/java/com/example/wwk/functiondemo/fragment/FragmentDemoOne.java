1c07ed1a0e37b4cc9954fc1e1ba3386ba330ea41
wc563388548@gmail.com <wc563388548@gmail.com>
2017-06-05 11:50:16 +0900

    Update FragmentDemoOne

--- app/src/main/java/com/example/wwk/functiondemo/fragment/FragmentDemoOne.java
+++ app/src/main/java/com/example/wwk/functiondemo/fragment/FragmentDemoOne.java
@@ -15,6 +15,11 @@ import com.example.wwk.functiondemo.R;
 import com.example.wwk.functiondemo.adapter.ChatBotAdapter;
 import com.example.wwk.functiondemo.entity.ChatBotData;
 import com.example.wwk.functiondemo.utils.L;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechSynthesizer;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.SynthesizerListener;
 import com.kymjs.rxvolley.RxVolley;
 import com.kymjs.rxvolley.client.HttpCallback;
 
@@ -23,6 +28,8 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static cn.bmob.v3.Bmob.getApplicationContext;
 
 /**
  * Created by wwk on 17/5/18.
@@ -41,14 +48,30 @@ public class FragmentDemoOne extends Fragment implements View.OnClickListener {
     private EditText mChatEditText;
     // Button of send a message
     private Button mSendButton;
+    //TTS
+    private SpeechSynthesizer mTts;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_demo_one, container, false);
+        SpeechUtility.createUtility(getApplicationContext(), SpeechConstant.APPID +"=5933f95b");
         initializeView(view);
         return view;
     }
 
     private void initializeView(View view) {
+
+        // 1.创建SpeechSynthesizer对象, 第二个参数：本地合成时传InitListener
+        mTts = SpeechSynthesizer.createSynthesizer(getActivity(), null);
+        // 2.合成参数设置，详见《科大讯飞MSC API手册(Android)》SpeechSynthesizer 类
+        // 设置发音人
+        mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan");
+        // 设置语速
+        mTts.setParameter(SpeechConstant.SPEED, "50");
+        // 设置音量，范围0~100
+        mTts.setParameter(SpeechConstant.VOLUME, "80");
+        // 设置云端
+        mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
 
         mChatListView = (ListView) view.findViewById(R.id.mChatListView);
         mChatEditText = (EditText) view.findViewById(R.id.input_message_edit);
@@ -58,7 +81,7 @@ public class FragmentDemoOne extends Fragment implements View.OnClickListener {
         // Set adapter
         adapter = new ChatBotAdapter(getActivity(), mList);
         mChatListView.setAdapter(adapter);
-        addLeftItem("你好，我是chat bot。");
+        addLeftItem("你好，我是管家。");
 
 
     }
@@ -113,6 +136,8 @@ public class FragmentDemoOne extends Fragment implements View.OnClickListener {
     // Add text to left
     private void addLeftItem(String text) {
 
+        startSpeak(text);
+
         ChatBotData data = new ChatBotData();
         data.setType(ChatBotAdapter.VALUE_LEFT_TEXT);
         data.setTextOfChat(text);
@@ -135,4 +160,43 @@ public class FragmentDemoOne extends Fragment implements View.OnClickListener {
         // Get to bottom
         mChatListView.setSelection(mChatListView.getBottom());
     }
+
+    // Start to speak
+    private void startSpeak(String text) {
+        // Start to compound
+        mTts.startSpeaking(text, mSynListener);
+    }
+
+    // 合成监听器
+    private SynthesizerListener mSynListener = new SynthesizerListener() {
+        //会话结束回调接口，没有错误时，error为null
+        public void onCompleted(SpeechError error) {
+        }
+
+        // 缓冲进度回调
+        //percent为缓冲进度0~100，beginPos为缓冲音频在文本中开始位置，endPos表示缓冲音频在文本中结束位置，info为附加信息。
+        public void onBufferProgress(int percent, int beginPos, int endPos, String info) {
+        }
+
+        // 开始播放
+        public void onSpeakBegin() {
+        }
+
+        // 暂停播放
+        public void onSpeakPaused() {
+        }
+
+        // 播放进度回调
+        // percent为播放进度0~100, beginPos为播放音频在文本中开始位置，endPos表示播放音频在文本中结束位置.
+        public void onSpeakProgress(int percent, int beginPos, int endPos) {
+        }
+
+        // 恢复播放回调接口
+        public void onSpeakResumed() {
+        }
+
+        // 会话事件回调接口
+        public void onEvent(int arg0, int arg1, int arg2, Bundle arg3) {
+        }
+    };
 }
