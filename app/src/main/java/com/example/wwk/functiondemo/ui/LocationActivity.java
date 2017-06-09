b5d19b9e3625276b6320d64c8ee65b2c63bd4611
wc563388548@gmail.com <wc563388548@gmail.com>
2017-06-09 10:15:32 +0900

    Update Location activity

--- app/src/main/java/com/example/wwk/functiondemo/ui/LocationActivity.java
+++ app/src/main/java/com/example/wwk/functiondemo/ui/LocationActivity.java
@@ -2,9 +2,18 @@ package com.example.wwk.functiondemo.ui;
 
 import android.os.Bundle;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
 import com.example.wwk.functiondemo.R;
+import com.example.wwk.functiondemo.utils.L;
+
+import java.util.List;
 
 /**
  * Created by wwk on 17/6/8.
@@ -15,6 +24,10 @@ import com.example.wwk.functiondemo.R;
 public class LocationActivity extends BaseActivity{
 
     private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    // Locating
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,7 +42,46 @@ public class LocationActivity extends BaseActivity{
 
     private void initializeView() {
         mMapView = (MapView) findViewById(R.id.mapview_location);
+        mBaiduMap = mMapView.getMap();
+        // Define LocationClient class
+        mLocationClient = new LocationClient(getApplicationContext());
+        // Register Listener function
+        mLocationClient.registerLocationListener(myListener);
 
+        initLocation();
+
+        // Start locating
+        mLocationClient.start();
+        L.error("Start locating");
+
+    }
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        // 可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        // 可选，默认gcj02，设置返回的定位结果坐标系
+        option.setCoorType("bd09ll");
+        int span = 0;
+        // 可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setScanSpan(span);
+        // 可选，设置是否需要地址信息，默认不需要
+        option.setIsNeedAddress(true);
+        // 可选，默认false,设置是否使用gps
+        option.setOpenGps(true);
+        // 可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setLocationNotify(true);
+        // 可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationDescribe(true);
+        // 可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIsNeedLocationPoiList(true);
+        // 可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.setIgnoreKillProcess(false);
+        // 可选，默认false，设置是否收集CRASH信息，默认收集
+        option.SetIgnoreCacheException(false);
+        // 可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        option.setEnableSimulateGps(false);
+        mLocationClient.setLocOption(option);
     }
 
     @Override
@@ -48,5 +100,87 @@ public class LocationActivity extends BaseActivity{
     protected void onPause() {
         super.onPause();
         mMapView.onPause();
+    }
+
+    // Callback of location
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // Receive Location
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            // GPS result
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                sb.append("\nspeed : ");
+                // km / h
+                sb.append(location.getSpeed());
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\nheight : ");
+                // meter
+                sb.append(location.getAltitude());
+                sb.append("\ndirection : ");
+                // degree
+                sb.append(location.getDirection());
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+                // Result of Internet
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                // Information of operator
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+                // Result of offline
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+            sb.append("\nlocationdescribe : ");
+            // 位置语义化信息
+            sb.append(location.getLocationDescribe());
+            // POI data
+            List<Poi> list = location.getPoiList();
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+            // Result
+            L.information(sb.toString());
+            L.error("Stopped locating");
+            
+        }
+
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+
+        }
     }
 }
